78ffd9da1b0042aca8818f27501ff9610e3340d9
eldunbaro <30471544+eldunbaro@users.noreply.github.com>
2017-07-27 18:03:28 +0100

    Added new console application to test DynamoDB.
    Console application added in order to learn about and test insertion of items into DynamoDB table.

---
+++ Calculator.Console/Program.cs
@@ -0,0 +1,26 @@
+﻿using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DocumentModel;
+using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string accessKey = "AKIAIP7GFUE2MSO6BDDQ";
+            string secretKey = "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";
+            var client = new AmazonDynamoDBClient(accessKey, secretKey, Amazon.RegionEndpoint.EUWest2);
+            var table = Table.LoadTable(client, "Calculation");
+            Document document = new Document();
+            document["CalculationId"] = Guid.NewGuid().ToString();
+            document["CalculationDate"] = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss");   // "2017-07-26T17:54:00";
+            table.PutItem(document);
+        }
+    }
+}
